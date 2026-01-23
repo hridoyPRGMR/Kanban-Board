5f90d45ee71a680aeade0db7ec0ff5084b650c75
hriody <hridoymia.prgmr@gmail.com>
2026-01-23 10:06:09 +0600

    Save work before updating to Angular 21

--- KanbanBoard.ClientApp/src/app/features/project-management/project-management.ts
+++ KanbanBoard.ClientApp/src/app/features/project-management/project-management.ts
@@ -5,11 +5,20 @@ import { Dashboard } from "../dashboard/dashboard";
 import { Tab } from '../../shared/common/tab/tab';
 import { TabPaneDirective } from '../../shared/common/tab/tab-pane.directive';
 import { Project } from "./project/project";
+import { ProjectCard } from 'src/app/shared/common/project-card/project-card';
 
 @Component({
   selector: 'app-project-management',
   standalone: true,
-  imports: [CommonModule, Tab, TabPaneDirective, Board, Dashboard, Project],
+  imports: [
+    CommonModule, 
+    Tab, 
+    TabPaneDirective, 
+    Board, 
+    Dashboard, 
+    Project,
+    ProjectCard
+  ],
   templateUrl: './project-management.html',
   styleUrl: './project-management.scss',
 })
---
+++ KanbanBoard.ClientApp/src/app/shared/common/project-card/project-card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [],
+  templateUrl: './project-card.html',
+})
+export class ProjectCard {
+
+}
